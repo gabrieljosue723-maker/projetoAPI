11d101119b237351617ab0485dddf4618e8a024a
Josué Gabriel <gabrieljosue723@gmail.com>
2026-07-28 15:44:47 +0100

    Preparando o deploy da API

--- apiJwt/app/Providers/AppServiceProvider.php
+++ apiJwt/app/Providers/AppServiceProvider.php
@@ -1,6 +1,8 @@
 <?php
 
 namespace App\Providers;
+use Illuminate\Routing\UrlGenerator;
+
 
 use Illuminate\Support\ServiceProvider;
 
@@ -17,8 +19,10 @@ class AppServiceProvider extends ServiceProvider
     /**
      * Bootstrap any application services.
      */
-    public function boot(): void
+    public function boot(UrlGenerator $url): void
     {
-        //
+        if (env('APP_ENV') === 'production') {
+            $url->forceScheme('https');
+        }
     }
 }
